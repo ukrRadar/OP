9d273dabccd16a7a0d442789d615bd042cb5b4e1
ukrRadar <127085225+ukrRadar@users.noreply.github.com>
2025-12-10 01:19:19 +0200

    Implement number system selection for conversion
    Added functionality to choose number system for conversion.

--- Lr10/6.c
+++ Lr10/6.c
@@ -13,11 +13,32 @@ int main()
         printf("Число %d: ", i + 1);
         scanf("%d", &numbers[i]);
     }
+
+    printf("\nВиберіть систему числення для конвертації:\n");
+    printf("8  - вісімкова\n");
+    printf("16 - шістнадцяткова\n");
+    printf("0  - всі системи\n");
+    printf("Ваш вибір: ");
+    
+    scanf("%d", &choice);
+
     printf("\nРезультати конвертації:\n");
     printf("========================================\n");
     for (int i = 0; i < N; i++) 
     {
-        printf("Десяткова: %d\t Вісімкова: %o\tШістнадцяткова: %X\n", numbers[i], numbers[i], numbers[i]);
+        printf("Десяткова: %d\t", numbers[i]);
+        if (choice == 8) 
+        {
+            printf("Вісімкова: %o\n", numbers[i]);
+        } 
+        else if (choice == 16) 
+        {
+            printf("Шістнадцяткова: %X\n", numbers[i]);
+        } 
+        else 
+        {
+            printf("Вісімкова: %o\tШістнадцяткова: %X\n", numbers[i], numbers[i]);
+        }
     }
     printf("========================================\n");
     return 0;
