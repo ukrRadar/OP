860795478013799d299a23c35505809e30b8a826
ukrRadar <127085225+ukrRadar@users.noreply.github.com>
2025-12-09 19:30:56 +0200

    Refactor input handling and display digits using segments

--- Lr10/1.c
+++ Lr10/1.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
-
-#define MAX_SIZE 100
-
-void readNChars(char *array, int n);
+#include <string.h>
 
 int main() 
 {
-    int N;
+    char digits[20];
+    printf("Введіть до 10 цілих чисел: ");
+    scanf("%s", digits);
 
-    printf("Введіть кількість сиволів N (max:%d): ", MAX_SIZE);
-    scanf("%d", &N);
-
-    getchar(); 
-    char array[MAX_SIZE];
-
-    printf("Введіть %d символів:\n", N);
-    readNChars(array, N);
-
-    printf("\nВи ввели:\n");
-    
-    for (int i = 0; array[i] != '\0'; i++) 
+    int seg[10][7] = 
     {
-        putchar(array[i]);
+        {1,1,1,1,1,1,0},
+        {0,1,1,0,0,0,0},
+        {1,1,0,1,1,0,1},
+        {1,1,1,1,0,0,1},
+        {0,1,1,0,0,1,1},
+        {1,0,1,1,0,1,1},
+        {1,0,1,1,1,1,1},
+        {1,1,1,0,0,0,0},
+        {1,1,1,1,1,1,1},
+        {1,1,1,1,0,1,1}
+    };
+    for (int i = 0; i < strlen(digits); i++) 
+    {
+        int d = digits[i] - '0';
+        printf(" ");
+        printf(seg[d][0] ? "_" : " ");
+        printf(" ");
     }
+    printf("\n");
+    for (int i = 0; i < strlen(digits); i++) 
+    {
+        int d = digits[i] - '0';
+        printf(seg[d][5] ? "|" : " ");
+        printf(seg[d][6] ? "_" : " ");
+        printf(seg[d][1] ? "|" : " ");
+    }
+    printf("\n");
+    for (int i = 0; i < strlen(digits); i++) 
+    {
+        int d = digits[i] - '0';
+        printf(seg[d][4] ? "|" : " ");
+        printf(seg[d][3] ? "_" : " ");
+        printf(seg[d][2] ? "|" : " ");
+    }
+    printf("\n");
 
     return 0;
-}
-void readNChars(char *array, int n) 
-{
-    int i = 0;
-    while (i < n) 
-    {
-        int c = getchar();
-        
-        if (c == EOF) 
-        {
-            break;
-        }
-        array[i++] = (char)c;
-    }
-    array[i] = '\0';
 }
